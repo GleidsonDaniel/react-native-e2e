544ba6a29a4e3bd4198be8ba600d146588af3b0f
Daniel <gleidson10daniel@hotmail.com>
2021-05-06 11:37:25 -0300

    Add brown color

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,22 +1,8 @@
 const colors = {
   cyan: '#05bcfa',
-  darkCyan: '#0496C8',
-  darkGray: '#545454',
-  darkShade: '#4B6484',
-  gray: '#ECECEC',
-  green: '#31CFA5',
-  greenDarkGreen: '#00B686',
-  lightCyan: '#E1F7FF',
-  lightShade: '#DDE3EA',
   mediumGray: '#979797',
-  shade: '#5E7BA1',
-  shadeDarkShade: '#4B6484',
-  tabBarUnselectedIcon: '#a3b0be',
-  white: '#FFFFFF',
   salmone: '#FA5961',
-  darkSunny: '#E8A700',
-  sunny: '#FFBC0D',
-  grayLightGray: '#BFBFBF',
+  brown: '#6c2e00',
 };
 
 export const theme = {
