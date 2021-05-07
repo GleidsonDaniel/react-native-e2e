fda4b82ebcc80216c17dea4e7afac7dc12e9ec7f
Daniel <gleidson10daniel@hotmail.com>
2021-05-07 08:28:42 -0300

    Add Login test

---
+++ e2e/login.spec.ts
@@ -0,0 +1,65 @@
+import {expect, device, element, by, waitFor} from 'detox';
+
+describe('Login Flow', () => {
+  beforeEach(async () => {
+    await device.installApp();
+  });
+  beforeEach(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have login screen components visible', async () => {
+    await expect(element(by.id('baseInput_email'))).toBeVisible();
+    await expect(element(by.id('baseInput_senha'))).toBeVisible();
+    await expect(element(by.id('login_button'))).toBeVisible();
+  });
+
+  it('verify email input', async () => {
+    await expect(element(by.id('baseInput_email'))).toBeVisible();
+    await element(by.id('baseInput_email')).typeText('danielhotmail.com');
+    await element(by.id('baseInput_email')).tapReturnKey();
+    await expect(element(by.id('errorMessage_email'))).toBeVisible();
+    await expect(element(by.id('errorMessage_email'))).toHaveText(
+      'Insira um email valido',
+    );
+    await element(by.id('baseInput_email')).clearText();
+    await element(by.id('baseInput_email')).typeText('daniel@hotmail.com');
+    await element(by.id('baseInput_email')).tapReturnKey();
+    await expect(element(by.id('errorMessage_email'))).not.toBeVisible();
+  });
+
+  it('verify password input', async () => {
+    await expect(element(by.id('baseInput_senha'))).toBeVisible();
+    await element(by.id('baseInput_senha')).typeText('1234567');
+    await element(by.id('baseInput_senha')).tapReturnKey();
+    await expect(element(by.id('errorMessage_senha'))).toBeVisible();
+    await expect(element(by.id('errorMessage_senha'))).toHaveText(
+      'A senha deve ter no minimo 8 caracteres',
+    );
+    await element(by.id('baseInput_senha')).clearText();
+    await element(by.id('baseInput_senha')).typeText('12345678');
+    await element(by.id('baseInput_senha')).tapReturnKey();
+    await expect(element(by.id('errorMessage_senha'))).not.toBeVisible();
+  });
+
+  it('test login flow', async () => {
+    await expect(element(by.id('baseInput_email'))).toBeVisible();
+    await element(by.id('baseInput_email')).typeText('daniel@hotmail.com');
+    await element(by.id('baseInput_email')).tapReturnKey();
+    await expect(element(by.id('baseInput_senha'))).toBeVisible();
+    await element(by.id('baseInput_senha')).typeText('12345678');
+    await element(by.id('baseInput_email')).tapReturnKey();
+    await expect(element(by.id('login_button'))).toBeVisible();
+    await element(by.id('login_button')).tap();
+    await waitFor(element(by.id('back_button')))
+      .toBeVisible()
+      .withTimeout(2000);
+    await expect(element(by.id('back_button'))).toBeVisible();
+    await element(by.id('back_button')).tap();
+    await expect(element(by.id('login_button'))).toBeVisible();
+  });
+});
